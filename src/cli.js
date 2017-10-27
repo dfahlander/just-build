fe6ea31c593cb81d1718466e5f742894352782db
David Fahlander <david.fahlander@gmail.com>
2017-10-28 00:03:35 +0200

    Don't complain about invalid memory leak

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 const {main} = require ('./index');
 const clr = require ('./console-colors');
 
+require('events').EventEmitter.prototype._maxListeners = 100;
+
 main(process.argv).then(()=>{
     process.exit(0);
 }).catch(err => {
