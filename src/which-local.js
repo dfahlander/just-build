39279f06a89535b98c54b29349f57c70d83419b0
dfahlander <david.fahlander@gmail.com>
2022-09-04 22:45:20 +0200

    Support for pnpm

--- src/which-local.js
+++ src/which-local.js
@@ -48,34 +48,17 @@ function whichLocal (cmd, cwd) {
     let splitted = binFilePath.split('/');
     while (splitted.length > 0 && (
         splitted[0].indexOf('$') === 0 || // $basedir
-        splitted[0] === '..' || // ".."
+        //splitted[0] === '..' || // ".."
         splitted[0] === '.')) // just for optional future changes
     {
         splitted = splitted.slice(1);
     }
 
-    const jsScriptPath = path.normalize(path.resolve(node_modules, splitted.join('/')));
+    const jsScriptPath = path.normalize(path.resolve(binDir, splitted.join('/')));
     debug.log(`jsScriptPath = ${JSON.stringify(jsScriptPath)}`);
     if (splitted.length === 0) return null;
-    const moduleName = splitted[0];
-    debug.log(`moduleName = ${JSON.stringify(moduleName)}`);
-    const packageJson = path.resolve(node_modules, moduleName, "package.json");
-    debug.log(`packageJson = ${JSON.stringify(packageJson)}`);
-    const binAttribute = JSON.parse(fs.readFileSync(packageJson)).bin;
-    debug.log(`binAttribute = ${JSON.stringify(binAttribute)}`);
-    const configureBinPath = path.resolve(node_modules, moduleName,
-        typeof binAttribute === 'string' ?
-            binAttribute : // "bin": "bin/cli"
-            binAttribute[cmd]); // "bin": {"cmd1": "bin/cmd1", "cmd2": ...}
     
-    if (path.normalize(configureBinPath) !== jsScriptPath) {
-        console.error(`Warning: fork optimization failed to to a sanity check. ` +
-            `Expected '${jsScriptPath} to equal configured bin path `+
-            `'${configureBinPath}'`);
-        return null;
-    }
-
-    return path.normalize(configureBinPath);
+    return jsScriptPath;
 }
 
 module.exports = { whichLocal };
